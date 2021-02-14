42381d63c9420b894fd7d512fab8eab1215263ed
Domantas Jurkus <domantas.jurkus@skyscanner.net>
2021-02-14 23:27:49 +0000

    add wsgi/main.py example for failed relative import

---
+++ wsgi/main.py
@@ -0,0 +1,8 @@
+import sys
+
+path = '/Users/domantasjurkus/Downloads/csnit/roganlog/roganlog_flask'
+
+if path not in sys.path:
+    sys.path.append(path)
+
+from __init__ import app as application
